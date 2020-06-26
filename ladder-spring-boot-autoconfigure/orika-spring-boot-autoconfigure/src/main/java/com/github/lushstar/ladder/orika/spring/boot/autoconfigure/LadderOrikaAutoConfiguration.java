7623bd485280c559987c4e5da002d27c54a64732
masteryourself <renzhao.ruan@foxmail.com>
2020-06-26 17:05:22 +0800

    bug fix

--- ladder-spring-boot-autoconfigure/orika-spring-boot-autoconfigure/src/main/java/com/github/lushstar/ladder/orika/spring/boot/autoconfigure/LadderOrikaAutoConfiguration.java
+++ ladder-spring-boot-autoconfigure/orika-spring-boot-autoconfigure/src/main/java/com/github/lushstar/ladder/orika/spring/boot/autoconfigure/LadderOrikaAutoConfiguration.java
@@ -3,8 +3,8 @@ package com.github.lushstar.ladder.orika.spring.boot.autoconfigure;
 import ma.glasnost.orika.MapperFacade;
 import ma.glasnost.orika.MapperFactory;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,13 +22,13 @@ import org.springframework.context.annotation.Configuration;
 public class LadderOrikaAutoConfiguration {
 
     @Bean
-    @ConditionalOnBean(MapperFactory.class)
+    @ConditionalOnMissingBean(MapperFactory.class)
     public MapperFactory getFactory() {
         return new DefaultMapperFactory.Builder().build();
     }
 
     @Bean
-    @ConditionalOnBean(MapperFacade.class)
+    @ConditionalOnMissingBean(MapperFacade.class)
     public MapperFacade mapperFacade() {
         return getFactory().getMapperFacade();
     }
