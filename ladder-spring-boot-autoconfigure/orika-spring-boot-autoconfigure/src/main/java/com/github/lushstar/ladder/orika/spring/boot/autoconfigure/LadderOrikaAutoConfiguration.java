789a90b9dbeb74162a3c5a2ce00479ab4ffe7828
masteryourself <renzhao.ruan@foxmail.com>
2020-06-26 16:49:17 +0800

    增加 orika

---
+++ ladder-spring-boot-autoconfigure/orika-spring-boot-autoconfigure/src/main/java/com/github/lushstar/ladder/orika/spring/boot/autoconfigure/LadderOrikaAutoConfiguration.java
@@ -0,0 +1,36 @@
+package com.github.lushstar.ladder.orika.spring.boot.autoconfigure;
+
+import ma.glasnost.orika.MapperFacade;
+import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.impl.DefaultMapperFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <p>description : LadderOrikaAutoConfiguration
+ *
+ * <p>blog : https://blog.csdn.net/masteryourself
+ *
+ * @author : masteryourself
+ * @version : 1.0.0
+ * @date : 2020/6/26 16:29
+ */
+@Configuration
+@ConditionalOnClass({MapperFactory.class})
+public class LadderOrikaAutoConfiguration {
+
+    @Bean
+    @ConditionalOnBean(MapperFactory.class)
+    public MapperFactory getFactory() {
+        return new DefaultMapperFactory.Builder().build();
+    }
+
+    @Bean
+    @ConditionalOnBean(MapperFacade.class)
+    public MapperFacade mapperFacade() {
+        return getFactory().getMapperFacade();
+    }
+
+}
